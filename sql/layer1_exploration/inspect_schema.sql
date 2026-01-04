67d1b98404f03760caa13a372a42a7ce5bffa5a6
suchitradivakaran-ai <suchitra.divakaran@gmail.com>
2026-01-04 14:48:20 +0530

    Update inspect_schema.sql

--- sql/layer1_exploration/inspect_schema.sql
+++ sql/layer1_exploration/inspect_schema.sql
@@ -1,1 +1,9 @@
+-- Purpose:
+-- Explore the top-level structure of the Google Analytics session dataset.
+-- This query is used during Layer 1 (Data Familiarisation) to understand
+-- available fields, nested structures, and data types before analysis.
+
+SELECT *
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+LIMIT 10;
 
