12527aa692fc24c024e5b56fbd99ded41136e17f
suchitradivakaran <suchitra.divakaran@gmail.com>
2026-01-06 18:01:00 +0530

    Update funnel_event_mapping.sql

--- sql/layer4_funnel/funnel_event_mapping.sql
+++ sql/layer4_funnel/funnel_event_mapping.sql
@@ -1,1 +1,16 @@
+-- Purpose:
+-- Explore event categories and actions to identify which events
+-- can be mapped to conceptual onboarding funnel stages.
 
+SELECT
+  h.eventInfo.eventCategory,
+  h.eventInfo.eventAction,
+  COUNT(*) AS event_count
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
+     UNNEST(hits) AS h
+WHERE _TABLE_SUFFIX BETWEEN '20170701' AND '20170707'
+  AND h.type = 'EVENT'
+GROUP BY
+  h.eventInfo.eventCategory,
+  h.eventInfo.eventAction
+ORDER BY event_count DESC;
